9dc9d2f0876aac7653d99bca880f9af2737d0374
ar.zhivitskij2011@yandex.by <ar.zhivitskij2011@yandex.by>
2019-10-11 17:10:53 +0300

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using QuickSort;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = new double[10];
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++) array[i] = random.Next(1, 50);
+
+            Console.WriteLine("Array before sorting: \n");
+
+            foreach (var arr in array) Console.Write($"{arr} ");
+
+            QuickSort.Sorting(array, 0, array.Length - 1);
+
+            Console.WriteLine("\n\nArray after sorting: \n");
+
+            foreach (var arr in array) Console.Write($"{arr} ");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSort
+{
+    class QuickSort
+    {
+        public static void Sorting(double[] array, long first, long last)
+        {
+            double element = array[(last - first) / 2 + first];
+            long i = first;
+            long j = last;
+
+            while (i <= j)
+            {
+                while (array[i] < element && i <= last) ++i;
+
+                while (array[j] > element && j >= first) --j;
+
+                if (i <= j)
+                {
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    ++i;
+                    --j;
+                }
+            }
+
+            if (j > first) Sorting(array, first, j);
+
+            if (i < last) Sorting(array, i, last);
+        }
+    }
+}
